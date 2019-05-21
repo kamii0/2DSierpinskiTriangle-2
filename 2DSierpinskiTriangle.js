8b590facc217d2f7bff8938afeae207bfb2db6bf
kamii0 <36809233+kamii0@users.noreply.github.com>
2019-05-21 13:30:43 -0400

    Add files via upload
    Updated with code

---
+++ 2DSierpinskiTriangle.js
@@ -0,0 +1,139 @@
+﻿"use strict";
+
+/*
+ * Course: CS 4722/7327
+ * Section: ..... I dont know the section number
+ * Name: Nikki Ashaka
+ * Professor: Shaw
+ * Assignment #: Lab 3
+ */
+
+
+var canvas;
+var gl;
+
+var points = [];
+
+var numTimesToSubdivide = 0;
+
+var bufferId;
+
+var color = vec4(1.0, 0.0, 0.0, 1.0);
+var colorLoc;
+
+
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) { alert("WebGL isn't available"); }
+
+    //
+    //  Initialize our data for the Sierpinski Gasket
+    //    
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    //  Load shaders and initialize attribute buffers
+
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    // Load the data into the GPU
+
+    bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, 8 * Math.pow(3, 6), gl.STATIC_DRAW);
+
+
+
+    // Associate out shader variables with our data buffer
+
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    colorLoc = gl.getUniformLocation(program, "color");
+    gl.uniform4fv(colorLoc, color);
+
+
+
+    document.getElementById("sliderval").onchange =
+        function (event) {
+            numTimesToSubdivide = Number(document.getElementById('sliderval').value);
+            if (numTimesToSubdivide == 0) {
+                color = vec4(1.0, 0.0, 0.0, 1.0);
+            }
+            else if (numTimesToSubdivide == 1) {
+                color = vec4(0.0, 1.0, 0.0, 1.0);
+            }
+            else if (numTimesToSubdivide == 2 ){
+                color = vec4(0.0, 0.0, 1.0, 1.0);
+            }
+            else if (numTimesToSubdivide == 3) {
+                color = vec4(1.0, 1.0, 0.0, 1.0);
+            }
+            else if (numTimesToSubdivide == 4) {
+                color = vec4(0.0, 1.0, 1.0, 1.0);
+            }
+            else {
+                color = vec4(1.0, 0.0, 1.0, 1.0);
+            }
+            // update the uniform variable
+            gl.uniform4fv(colorLoc, color);
+            render();
+        };
+
+
+    render();
+};
+
+
+function triangle(a, b, c) {
+    points.push(a, b, c);
+}
+
+function divideTriangle(a, b, c, count) {
+
+    // check for end of recursion
+
+    if (count == 0) {
+        triangle(a, b, c);
+    }
+    else {
+
+        //bisect the sides
+
+        var ab = mix(a, b, 0.5);
+        var ac = mix(a, c, 0.5);
+        var bc = mix(b, c, 0.5);
+
+        --count;
+
+        // three new triangles
+        divideTriangle(a, ab, ac, count);
+        divideTriangle(c, ac, bc, count);
+        divideTriangle(b, bc, ab, count);
+    }
+}
+
+
+function render() {
+    var vertices = [
+        vec2(-1, -1),
+        vec2(0, 1),
+        vec2(1, -1)
+    ];
+    points = [];
+    divideTriangle(vertices[0], vertices[1], vertices[2],
+        numTimesToSubdivide);
+
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, points.length);
+    points = [];
+}
